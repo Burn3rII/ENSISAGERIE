cfe6004d1a4697bd988dc6d4122ab595ffb31a08
Simon <antropius.simon00@gmail.com>
2023-12-13 10:11:09 +0100

    Test commit après ajout pull request only.

--- messagingsite/messagingsite/settings.py
+++ messagingsite/messagingsite/settings.py
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'users.apps.UsersConfig',  # Important que l'app users soit avant les
+    # contrib.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'users:dashboard'
+LOGOUT_REDIRECT_URL = 'users:dashboard'
+
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
