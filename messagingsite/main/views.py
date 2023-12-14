e319d6d1fedbe7de9ebc3c3855f98b23ed91264d
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-14 13:59:17 +0100

    Ajouter le CDN jquery

--- messagingsite/main/views.py
+++ messagingsite/main/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
-from django.contrib.auth.views import (LoginView, LogoutView,
-                                       PasswordChangeView,
-                                       PasswordChangeDoneView,
-                                       PasswordResetView,
-                                       PasswordResetDoneView,
-                                       PasswordResetConfirmView,
-                                       PasswordResetCompleteView)
-
 
 class HomeView(TemplateView):
     template_name = "main/index.html"
