742ca974546d4c65897cf584277eaaf37cbc646b
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-15 11:19:03 +0100

    Update script.js

--- messagingsite/static/script.js
+++ messagingsite/static/script.js
@@ -1,3 +1,34 @@
+window.onload = function(){
+    var theme = localStorage.getItem('theme')
+    switch (theme) {
+        case 'dark-theme':
+            document.body.classList.toggle("dark-theme");
+            localStorage.setItem("theme","dark-theme")
+            break;
+        case 'light-theme':
+            localStorage.setItem("theme","light-theme")
+            break;
+        default:
+            localStorage.setItem("theme","light-theme")
+    }
+}
+
 $("#theme-switch").click(function(){
+    var theme = localStorage.getItem('theme')
     document.body.classList.toggle("dark-theme");
+    switch (theme) {
+        case 'dark-theme':
+            localStorage.setItem("theme","light-theme")
+            //theme = localStorage.getItem('theme')
+            //console.log(theme)
+            break;
+        case 'light-theme':
+            localStorage.setItem("theme","dark-theme")
+            //theme = localStorage.getItem('theme')
+            //console.log(theme)
+            break;
+        //default:
+        //    localStorage.setItem("theme","light-theme")
+    //document.body.classList.toggle("dark-theme");
+    }
 })
