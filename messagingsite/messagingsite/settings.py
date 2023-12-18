8f90296788ab182764fb646d56801e726ef8e855
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-18 19:12:06 +0100

    Base error

--- messagingsite/messagingsite/settings.py
+++ messagingsite/messagingsite/settings.py
@@ -125,6 +125,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
