6bb9254b7a432f9ff7c1452e5b7dc3c6b2ccc22e
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2023-12-18 21:07:58 +0100

    patch pour error

--- messagingsite/messagingsite/settings.py
+++ messagingsite/messagingsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8a@e7y2-a!5)vkuq!&509hu_h1d$!#gv*zicwpm#tf%(7nprtr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,3 +145,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # réels tout en permettant aux développeurs de voir les e-mails générés
 # directement dans la console.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# whitenoise config
+
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
